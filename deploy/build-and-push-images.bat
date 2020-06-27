6ba572fc3cf14aa8a9c47e3231ec738a8ffebf1e
Otávio Nielsen <otavio.juliano.ns@gmail.com>
2020-06-27 15:54:50 -0300

    update

--- deploy/build-and-push-images.bat
+++ deploy/build-and-push-images.bat
@@ -1,17 +1,15 @@
-::@echo off
 cd ..\src
 docker login
 docker build --no-cache -t otaviojulianons/api-gateway:1.0.3 -f APIGateway/Dockerfile .
-docker build --no-cache -t otaviojulianons/api-resizer:1.0.3 -f Resizer/Dockerfile .
-docker build --no-cache -t otaviojulianons/api-customer:1.0.3 -f Customers/Dockerfile .
-docker build --no-cache -t otaviojulianons/api-fileserver:1.0.3 -f FileServer/Dockerfile .
+docker build --no-cache -t otaviojulianons/api-resizer:1.0.4 -f Resizer/Dockerfile .
+docker build --no-cache -t otaviojulianons/api-customer:1.0.4 -f Customers/Dockerfile .
+docker build --no-cache -t otaviojulianons/api-fileserver:1.0.4 -f FileServer/Dockerfile .
 
 docker push otaviojulianons/api-gateway:1.0.3
-docker push otaviojulianons/api-resizer:1.0.3
-docker push otaviojulianons/api-customer:1.0.3
-docker push otaviojulianons/api-fileserver:1.0.3
+docker push otaviojulianons/api-resizer:1.0.4
+docker push otaviojulianons/api-customer:1.0.4
+docker push otaviojulianons/api-fileserver:1.0.4
 
 cd ..\deploy
-pause
 
 
